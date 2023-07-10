6eb4a1e7f2d68cb585a31553da18290439218ccd
alexixx <al3314@yandex.ru>
2023-07-10 18:16:29 +0300

    Refactoring App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import './scss/app.scss';
 
-import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-
-import Header from './components/Header';
-
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Singleplayer from './pages/Singleplayer';
-import Multiplayer from './pages/Multiplayer';
 import Welcome from './components/Welcome';
 
 function App() {
