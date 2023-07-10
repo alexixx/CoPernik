2f70d85d5d3120ace5638b0706624c7025a4e132
alexixx <al3314@yandex.ru>
2023-07-10 18:00:20 +0300

    Refactoring Panorama

--- src/components/Panorama/index.tsx
+++ src/components/Panorama/index.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Panorama, YMaps } from 'react-yandex-maps';
-import axios from 'axios';
 import { RootState } from '../../redux/store';
 import Loader from '../Loader';
 
@@ -14,27 +13,17 @@ const BigPanorama = () => {
 
   const citiesCoords = useSelector((state: RootState) => state.cities.citiesCoords);
   const status = useSelector((state: RootState) => state.cities.status);
-
   const currentPoints = useSelector((state: RootState) => state.points.currentCoords);
   const currentLvl = useSelector((state: RootState) => state.player.level);
 
-  const [loaderPanorama, setLoaderPanorama] = useState(true);
-
-  //   useEffect(() => {
-  //     first
-
-  //   }, [currentPoints])
-
-  //Set next lvl
   useEffect(() => {
-    console.log('Переход на первый уровень');
+    // Moving to the next level
 
     dispatch(setNextLevel());
   }, []);
 
   useEffect(() => {
-    console.log('Произошло обновление уровня');
-    console.log('currentLvl: ', currentLvl);
+    console.log("There's been a level update");
     if (currentLvl) {
       getCity();
     }
@@ -42,8 +31,6 @@ const BigPanorama = () => {
 
   // After receiving the polygons, send them to the array
   useEffect(() => {
-    console.log(citiesCoords);
-
     if (status == 'success') {
       console.log('Запрос выполнен выполняется вызов функции setPoints');
 
@@ -53,8 +40,7 @@ const BigPanorama = () => {
 
   const getCity = async () => {
     // Get all cities for game
-
-    console.log('Выполняется getCity');
+    console.log('"getCity" is executed');
 
     try {
       // @ts-ignore
@@ -66,11 +52,9 @@ const BigPanorama = () => {
 
   const setPoints = () => {
     // Set coordinates of the selected panorama
-    console.log('Выполняется setPoints');
+    console.log('"setPoints" is executed');
 
-    console.log('🚀 ~ file: index.tsx:65 ~ setPoints ~ citiesCoords', citiesCoords);
     if (citiesCoords) {
-      // let indexCity = getRandomNumbers(0, 1, 0);
       let indexCity = 0;
       dispatch(
         setCurrentPoints([
@@ -87,9 +71,6 @@ const BigPanorama = () => {
         ]),
       );
     }
-
-    // dispatch(setCurrentPoints([getRandomNumbers(55.7, 55.8), getRandomNumbers(37.5, 37.6)]));
-    // dispatch(setCurrentPoints([getRandomNumbers(40.7, 60.8), getRandomNumbers(27.5, 47.6)]));
   };
 
   const getRandomNumbers = (min: number, max: number, float: number) => {
@@ -97,55 +78,21 @@ const BigPanorama = () => {
     return Number((Math.random() * (max - min) + min).toFixed(float));
   };
 
-  const onDirectionChange = () => {
-    console.log('Изменились координаты панорамы');
-  };
-  const onPanoramaError = (e: any) => {
-    console.log(e);
-    console.log('!!!Error was found');
-  };
   const onPanoramaChange = () => {
-    console.log('!!Coords of panorama is changed');
-
     setTimeout(() => {
       let panoramaDOM = document.querySelectorAll('.panorama-main *');
       if (panoramaDOM.length <= 1) {
-        // dispatch(setCurrentPoints([55.658412, 37.788544]));
         setPoints();
       }
       console.log(panoramaDOM);
     }, 2000);
   };
-  const onPanoramaLoad = (e: any) => {
-    // if (!e.panorama.isSupported()) {
-    //   console.log('Not supported!');
-    //   return;
-    // } else {
-    //   console.log('Supported!');
-    // }
-    // console.log('!!!Panorama is loaded');
-    // setTimeout(() => {
-    //   let panoramaDOM = document.querySelectorAll('.panorama-main *');
-    //   if (panoramaDOM.length <= 1) {
-    //     setPoints();
-    //   }
-    //   console.log(panoramaDOM);
-    // }, 2000);
-  };
 
   if (!currentPoints) {
-    console.log('Координаты для инициализации панорамы еще не получены');
-
     return <Loader name="main" />;
   } else {
-    // console.log('currentPoints ', currentPoints);
-
-    console.log('Производится рендер компонента панорамы');
-
+    console.log('The panorama is rendered');
     return (
-      // <>
-      //   <div className="test">{currentPoints}</div>
-      // </>
       <>
         <YMaps
           key={`panorama-${currentPoints}`}
@@ -160,9 +107,7 @@ const BigPanorama = () => {
               options={{ controls: [''], _markers: [''] }}
               width={'100%'}
               height={'100%'}
-              onLoad={(e) => onPanoramaLoad(e)}
               onPanoramaChange={onPanoramaChange()}
-              // onDirectionChange={() => onDirectionChange()}
             />
           </div>
         </YMaps>
